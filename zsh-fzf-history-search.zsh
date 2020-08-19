1eb912521a495462b02a3474902e068caae4d696
Josh Skidmore <josh@josh.sc>
2020-08-19 06:39:46 -0400

    exit if there is no history

--- zsh-fzf-history-search.zsh
+++ zsh-fzf-history-search.zsh
@@ -1,5 +1,9 @@
 fzf_history_seach() {
-  history -i 0 | fzf +s +m -x --tac -e | awk '{print substr($0, index($0, $4))}' | tr -d "\n"
+  local _history=$(history -i 0)
+
+  [[ -z "$_history" ]] && exit
+
+  echo -e "$_history"| fzf +s +m -x --tac -e | awk '{print substr($0, index($0, $4))}' | tr -d "\n"
 }
 
 autoload fzf_history_seach
