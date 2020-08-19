d5948462970a56b8dfb106e43903247b1726bb4c
Josh Skidmore <josh@josh.sc>
2020-08-19 08:13:18 -0400

    initiate fzf search with $BUFFER (if available)

--- zsh-fzf-history-search.zsh
+++ zsh-fzf-history-search.zsh
@@ -1,5 +1,5 @@
 fzf_history_seach() {
-  BUFFER=$(history -t '%Y-%m-%d %H:%M:%S' 0 | grep -v 1969 | fzf +s +m -x --tac -e | awk '{print substr($0, index($0, $4))}')
+  BUFFER=$(history -t '%Y-%m-%d %H:%M:%S' 0 | grep -v 1969 | fzf +s +m -x --tac -e -q "$BUFFER" | awk '{print substr($0, index($0, $4))}')
   zle end-of-line
 }
 
