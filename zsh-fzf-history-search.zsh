0564606b88f08c6adab9bc1f5a40cfa0e84396cf
Josh Skidmore <josh@josh.sc>
2020-08-19 06:48:11 -0400

    rework exist if there is no history

--- zsh-fzf-history-search.zsh
+++ zsh-fzf-history-search.zsh
@@ -1,9 +1,5 @@
 fzf_history_seach() {
-  local _history=$(history -i 0)
-
-  [[ -z "$_history" ]] && exit
-
-  echo -e "$_history"| fzf +s +m -x --tac -e | awk '{print substr($0, index($0, $4))}' | tr -d "\n"
+  history -i 0 | grep -v 1969 | fzf +s +m -x --tac -e | awk '{print substr($0, index($0, $4))}' | tr -d "\n"
 }
 
 autoload fzf_history_seach
